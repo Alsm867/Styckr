3ed1135c9eda98c3b8c497c02f11cb249f1bf4b7
sam <ortegachan867@gmail.com>
2021-10-07 16:08:23 -0400

    sign up form done

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -16,6 +16,21 @@ const removeUser = () => {
   };
 };
 
+export const signup = (user) => async (dispatch) => {
+    const { username, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+  };
+
 export const restoreUser = () => async dispatch => {
     const response = await csrfFetch('/api/session');
     const data = await response.json();
