12d23038bf164cb9c0243358fc9760babc003b8e
sam <ortegachan867@gmail.com>
2021-10-07 15:57:29 -0400

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import LoginFormPage from './components/LoginFormPage';
+
+
+
+function App() {
+  return (
+    <Switch>
+      <Route path="/login">
+        <LoginFormPage />
+      </Route>
+    </Switch>
+  );
+}
+
+
+export default App;
