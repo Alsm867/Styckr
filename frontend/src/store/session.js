4831778ece0b536e4e241aea1febcd7d810e13c8
sam <ortegachan867@gmail.com>
2021-10-07 16:54:55 -0400

    initial set up done

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -16,6 +16,14 @@ const removeUser = () => {
   };
 };
 
+export const logout = () => async (dispatch) => {
+    const response = await csrfFetch('/api/session', {
+      method: 'DELETE',
+    });
+    dispatch(removeUser());
+    return response;
+  };
+
 export const signup = (user) => async (dispatch) => {
     const { username, email, password } = user;
     const response = await csrfFetch("/api/users", {
