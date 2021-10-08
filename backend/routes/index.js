0bc16f794512c52882ef3e309816bf9edb151818
sam <ortegachan867@gmail.com>
2021-10-08 12:51:15 -0400

    setting up heroku

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,20 +1,18 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const apiRouter = require("./api");
+const apiRouter = require('./api');
 
-// router.get('/hello/world', function(req, res) {
-//   res.cookie('XSRF-TOKEN', req.csrfToken());
-//   res.send('Hello World!');
-// });
+router.use('/api', apiRouter);
+
 // Static routes
 // Serve React build files in production
-if (process.env.NODE_ENV === "production") {
-  const path = require("path");
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
   // Serve the frontend's index.html file at the root route
-  router.get("/", (req, res) => {
-    res.cookie("XSRF-TOKEN", req.csrfToken());
-    return res.sendFile(
-      path.resolve(__dirname, "../../frontend", "build", "index.html")
+  router.get('/', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
   });
 
@@ -23,21 +21,19 @@ if (process.env.NODE_ENV === "production") {
 
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
-    res.cookie("XSRF-TOKEN", req.csrfToken());
-    return res.sendFile(
-      path.resolve(__dirname, "../../frontend", "build", "index.html")
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
   });
 }
 
 // Add a XSRF-TOKEN cookie in development
-if (process.env.NODE_ENV !== "production") {
-  router.get("/api/csrf/restore", (req, res) => {
-    res.cookie("XSRF-TOKEN", req.csrfToken());
-    return res.json({});
+if (process.env.NODE_ENV !== 'production') {
+  router.get('/api/csrf/restore', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.status(201).json({});
   });
 }
-
-router.use("/api", apiRouter);
 
 module.exports = router;
