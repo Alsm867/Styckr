060f8c43ddfa4fcf03ca95eba27f41f5a9d47316
sam <ortegachan867@gmail.com>
2021-10-14 19:05:38 -0400

    got my buttons working for navigation to images and profile images almost everything is styled

---
+++ frontend/src/components/FooterComponent/index.js
@@ -0,0 +1,14 @@
+import './FooterComponent.css';
+
+
+function FooterComponent() {
+
+
+    return (
+        <div className="Footer">
+            <img className='git' src='https://res.cloudinary.com/dzjkwepju/image/upload/v1634251277/Styckr/zzzzzz_tfz2rt.png' alt='github'/><a className="my-hub"href="https://github.com/Alsm867">Sam Ortega</a>
+        </div>
+    );
+}
+
+export default FooterComponent;
